36d11bc53a41d6e1bf158ad5395e969e7461a294
Swapnil Mohite <swapnilmohite9565@gmail.com>
2024-09-06 18:09:24 +0530

    code updated

--- ToDo_List/todo.js
+++ ToDo_List/todo.js
@@ -10,23 +10,35 @@ addBtn.addEventListener("click",()=>{
     else{
         let li=document.createElement("li");
         ul.appendChild(li);
+        
+        let checkBox=document.createElement("input");
+        checkBox.setAttribute("type","checkbox");
+        li.appendChild(checkBox);
+
         let disabledInput=document.createElement("input");
         disabledInput.setAttribute("type","text");
         disabledInput.disabled=true;
         li.appendChild(disabledInput);
-
+        
         let removeBtn=document.createElement("button");
         removeBtn.textContent="Remove";
         li.appendChild(removeBtn);
  
         disabledInput.value=userInput.value;
         userInput.value="";
-        
+          
+        checkBox.addEventListener("change",()=>{
+            if(checkBox.checked){
+                disabledInput.style.textDecoration="line-through";
+                disabledInput.style.backgroundColor="#ffffffc4";
+            }
+            else{
+                disabledInput.style.textDecoration="none";
+                disabledInput.style.backgroundColor="white";
+            }
+        })
         removeBtn.addEventListener("click",()=>{
-            let value=disabledInput;
-            disabledInput.style.backgroundColor="#ffffffc4";
-            removeBtn.style.backgroundColor="rgb(255 0 21 / 55%)";
-            value.style.textDecoration="line-through";
+            li.remove();
         })
               
     }
